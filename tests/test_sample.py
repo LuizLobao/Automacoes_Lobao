2007eaedf33275b2f7022d5e0c6439ee9d7df71d
LuizLobao <luiz@lobao.com>
2023-10-26 20:23:09 -0300

    criação do requirements

---
+++ tests/test_sample.py
@@ -0,0 +1,13 @@
+from auto_lobao.funcoes import *
+from auto_lobao.modelo_tendencia import *
+from auto_lobao.modelo_tendencia import *
+
+
+
+def test_meu_primeiro_teste():
+    assert 1 == 1
+
+
+def test_meu_segundo_teste():
+    assert 2 == 2
+
