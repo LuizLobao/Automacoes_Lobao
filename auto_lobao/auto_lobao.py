2906784f15c6b61f9b284428e15c986d10e3176a
LuizLobao <luiz@lobao.com>
2023-10-26 18:44:10 -0300

    formatação usando autopep8

--- auto_lobao/auto_lobao.py
+++ auto_lobao/auto_lobao.py
@@ -7,94 +7,104 @@ import segredos
 from menu import *
 from modelo_tendencia import *
 
-warnings.filterwarnings('ignore', message="pandas only supports SQLAlchemy connectable .*")
+warnings.filterwarnings(
+    'ignore', message="pandas only supports SQLAlchemy connectable .*")
 
 user_id = os.getlogin()
-FORMAT = f'%(asctime)s | %(levelname)s | %(filename)s | User: {user_id} | %(message)s'
-logging.basicConfig(level=logging.INFO, filename="logs/auto_lobao.log", format=FORMAT)
+FORMAT = f'%(asctime)s | %(levelname)s | %(filename)s | User: {
+    user_id} | %(message)s'
+logging.basicConfig(level=logging.INFO,
+                    filename="logs/auto_lobao.log", format=FORMAT)
 
 
-data_referencia = (datetime.today()- timedelta(days=1))
-AAAAMMDD_referencia = (datetime.today()- timedelta(days=0)).strftime('%Y%m%d') 
+data_referencia = (datetime.today() - timedelta(days=1))
+AAAAMMDD_referencia = (datetime.today() - timedelta(days=0)).strftime('%Y%m%d')
 AAAAMM = (datetime.today()).strftime('%Y%m')
-ultimo_dia_do_mes = (data_referencia.replace(day=1) + timedelta(days=32)).replace(day=1) - timedelta(days=1)
+ultimo_dia_do_mes = (data_referencia.replace(day=1) +
+                     timedelta(days=32)).replace(day=1) - timedelta(days=1)
 dias_faltando = (ultimo_dia_do_mes - data_referencia).days
-hoje = (datetime.today()- timedelta(days=0)).strftime('%d/%m/%Y') 
+hoje = (datetime.today() - timedelta(days=0)).strftime('%d/%m/%Y')
 
 
 def roda_modelo_tendencia():
-	puxa_deflac_ref()
-	df_real = puxa_dados_real()
-	executa_procedure_sql('SP_CDO_PREPARA_BASE_TEND_VL')
+    puxa_deflac_ref()
+    df_real = puxa_dados_real()
+    executa_procedure_sql('SP_CDO_PREPARA_BASE_TEND_VL')
 
-	df = puxa_dados_para_simular()
-	produtos = ['FIBRA', 'NOVA FIBRA']
-	segmentos = ['VAREJO', 'EMPRESARIAL']
-	gestao = ['RSE', 'RCS', 'RNN', 'TLV', 'WEB', 'OUTROS NACIONAIS']
+    df = puxa_dados_para_simular()
+    produtos = ['FIBRA', 'NOVA FIBRA']
+    segmentos = ['VAREJO', 'EMPRESARIAL']
+    gestao = ['RSE', 'RCS', 'RNN', 'TLV', 'WEB', 'OUTROS NACIONAIS']
 
-	for produto in produtos:
-		for segmento in segmentos:
-			for gest in gestao:
-				filtraDF_e_CalculaTendencia(df, 'VL', produto, segmento, gest)
+    for produto in produtos:
+        for segmento in segmentos:
+            for gest in gestao:
+                filtraDF_e_CalculaTendencia(df, 'VL', produto, segmento, gest)
 
-	executa_procedure_sql('SP_CDO_PREPARA_BASE_TEND_VL_VLL')
-	montaExcelTendVlVll()
-	executa_procedure_sql('SP_CDO_TEND_VL_VLL_LEGADA_IGUAL_CDO')
+    executa_procedure_sql('SP_CDO_PREPARA_BASE_TEND_VL_VLL')
+    montaExcelTendVlVll()
+    executa_procedure_sql('SP_CDO_TEND_VL_VLL_LEGADA_IGUAL_CDO')
+
 
 def roda_verifica_duplicidade_bov():
-	caminho = 'C:\\JETL\\BASE\\old\\'
-	arquivos_colunas = [
-		('BOV_1059_20231025.TXT.zip','BOV_1059.TXT', 'NUMERO_PEDIDO'),
-		('BOV_1065_20231025.TXT.zip','BOV_1065.TXT', 'NUMERO_PEDIDO'),
-		('BOV_6163_20231025.TXT.zip','BOV_6163.TXT', 'cd_item_ordem'),
-		('BOV_6162_20231025.TXT.zip', 'BOV_6162.TXT', 'cd_item_ordem')
-	]
-	
-	for arquivo_zip, arquivo, coluna in arquivos_colunas:
-		verifica_duplicidade_bov(f'{caminho}{arquivo_zip}', arquivo, coluna)
+    caminho = 'C:\\JETL\\BASE\\old\\'
+    arquivos_colunas = [
+        ('BOV_1059_20231025.TXT.zip', 'BOV_1059.TXT', 'NUMERO_PEDIDO'),
+        ('BOV_1065_20231025.TXT.zip', 'BOV_1065.TXT', 'NUMERO_PEDIDO'),
+        ('BOV_6163_20231025.TXT.zip', 'BOV_6163.TXT', 'cd_item_ordem'),
+        ('BOV_6162_20231025.TXT.zip', 'BOV_6162.TXT', 'cd_item_ordem')
+    ]
 
-def enviar_email_tend_vl_vll_para_operacoes ():
-	
-	para = segredos.lista_email_vll_nf_to
-	cc = segredos.lista_email_vll_nf_cc
-	assunto = f"Projeção VL e VLL - FIBRA e NOVA FIBRA - {hoje}"
-	anexo = (f'S:\\Resultados\\01_Relatorio Diario\\1 - Base Eventos\\02 - TENDÊNCIA\\Insumos_Tendência\\Tend_VL_VLL_Fibra_NovaFibra_{AAAAMMDD}.xlsx')
-	corpo = f"""
-    <p>Caros,</p>
+    for arquivo_zip, arquivo, coluna in arquivos_colunas:
+        verifica_duplicidade_bov(f'{caminho}{arquivo_zip}', arquivo, coluna)
 
-    <p>Segue o arquivo atualizado com a projeção de VL e de VLL para Fibra Legado e Nova Fibra calculada hoje: {hoje}</p>
-    <p></p>
-    <p></p>
 
-    <p>Att,</p>
-    <p>Lobão, Luiz</p>
-    """
-	enviaEmailComAnexo(para, assunto, corpo, cc, anexo)
+def enviar_email_tend_vl_vll_para_operacoes():
 
-def libera_tendencias_processo_legado ():
-	logging.info('Libera tendencia do processo legado.')
-	comando_sql = 'update TB_VALIDA_CARGA_TENDENCIA set DATA_CARGA = convert(varchar, getdate(), 120 )'
-	executar_sql(comando_sql)
+    para = segredos.lista_email_vll_nf_to
+    cc = segredos.lista_email_vll_nf_cc
+    assunto = f"Projeção VL e VLL - FIBRA e NOVA FIBRA - {hoje}"
+    anexo = (
+        f'S:\\Resultados\\01_Relatorio Diario\\1 - Base Eventos\\02 - TENDÊNCIA\\Insumos_Tendência\\Tend_VL_VLL_Fibra_NovaFibra_{AAAAMMDD}.xlsx')
+    corpo = f"""
+<p>Caros,</p>
 
-def tendencias_para_tabela_historico ():
-	logging.info('Envia tendencia para tabela historico.')
-	nome_procedure = 'dbo.SP_CDO_0003_TENDENCIA_PARA_HISTORICO'
-	param = AAAAMM
-	executa_procedure_sql(nome_procedure, param=None)
+<p>Segue o arquivo atualizado com a projeção de VL e de VLL para Fibra Legado e Nova Fibra calculada hoje: {hoje}</p>
+<p></p>
+<p></p>
 
-def enviar_lista_pdv_outros ():
-	logging.info('Envia lista pdv OUTROS.')
-	comando_sql = 'select * from [VW_COD_SAP_OUTROS] order by qtd desc'
+<p>Att,</p>
+<p>Lobão, Luiz</p>
+"""
+    enviaEmailComAnexo(para, assunto, corpo, cc, anexo)
 
-	conexao = criar_conexao()
-	cursor = conexao.cursor()
-	df=pd.read_sql(comando_sql, conexao)
-	os.makedirs('temp', exist_ok=True)
-	df.to_csv('temp/pdv_outros.csv', sep=';', decimal=',') 
 
-	anexo = r'C:\Users\oi066724\Documents\Python\Automacoes_Lobao\temp\pdv_outros.csv'
+def libera_tendencias_processo_legado():
+    logging.info('Libera tendencia do processo legado.')
+    comando_sql = 'update TB_VALIDA_CARGA_TENDENCIA set DATA_CARGA = convert(varchar, getdate(), 120 )'
+    executar_sql(comando_sql)
 
-	corpo = """
+
+def tendencias_para_tabela_historico():
+    logging.info('Envia tendencia para tabela historico.')
+    nome_procedure = 'dbo.SP_CDO_0003_TENDENCIA_PARA_HISTORICO'
+    param = AAAAMM
+    executa_procedure_sql(nome_procedure, param=None)
+
+
+def enviar_lista_pdv_outros():
+    logging.info('Envia lista pdv OUTROS.')
+    comando_sql = 'select * from [VW_COD_SAP_OUTROS] order by qtd desc'
+
+    conexao = criar_conexao()
+    cursor = conexao.cursor()
+    df = pd.read_sql(comando_sql, conexao)
+    os.makedirs('temp', exist_ok=True)
+    df.to_csv('temp/pdv_outros.csv', sep=';', decimal=',')
+
+    anexo = r'C:\Users\oi066724\Documents\Python\Automacoes_Lobao\temp\pdv_outros.csv'
+
+    corpo = """
 		<div>
 			Caros, Segue a lista de PDVs que estão aparecendo como OUTROS na BOV.
 			Favor verificar e atualizar a classificação dos mesmos.
@@ -102,115 +112,120 @@ def enviar_lista_pdv_outros ():
 		<br>
 		<br>
 	"""
-	para = segredos.lista_email_pdv_outros
-	assunto = f'PDVs Outros: {hoje}!'
+    para = segredos.lista_email_pdv_outros
+    assunto = f'PDVs Outros: {hoje}!'
 
-	enviaEmailComAnexo(para, assunto, corpo, EmailCC=None, File = anexo)
+    enviaEmailComAnexo(para, assunto, corpo, EmailCC=None, File=anexo)
+
 
 def rotinas_receita_contratada():
-	proc = 'SP_PC_Update_Ticket_Fibra_VAREJO_Tendencia_porRegiao'
-	executa_procedure_sql(proc, AAAAMM)
-	proc = 'SP_PC_Update_Ticket_Fibra_EMPRESARIAL_Tendencia_porRegiao_IndCombo'
-	executa_procedure_sql(proc, AAAAMM)
-	#proc = 'SP_PC_Update_Ticket_Fibra_VAREJO_DIARIO_porRegiao'
-	#executa_procedure_sql(proc, AAAAMM)
-	#proc = 'SP_PC_Update_Ticket_Fibra_EMPRESARIAL_DIARIO_porRegiao_IndCombo'
-	#executa_procedure_sql(proc, AAAAMM)
-	proc = 'SP_PC_TBL_RE_RELATORIO_RC_V2_TEND'
-	executa_procedure_sql(proc, AAAAMM)
+    proc = 'SP_PC_Update_Ticket_Fibra_VAREJO_Tendencia_porRegiao'
+    executa_procedure_sql(proc, AAAAMM)
+    proc = 'SP_PC_Update_Ticket_Fibra_EMPRESARIAL_Tendencia_porRegiao_IndCombo'
+    executa_procedure_sql(proc, AAAAMM)
+    # proc = 'SP_PC_Update_Ticket_Fibra_VAREJO_DIARIO_porRegiao'
+    # executa_procedure_sql(proc, AAAAMM)
+    # proc = 'SP_PC_Update_Ticket_Fibra_EMPRESARIAL_DIARIO_porRegiao_IndCombo'
+    # executa_procedure_sql(proc, AAAAMM)
+    proc = 'SP_PC_TBL_RE_RELATORIO_RC_V2_TEND'
+    executa_procedure_sql(proc, AAAAMM)
+
 
 def criar_arquivo_ofertas_vs_depara_ticket():
-	logging.info('Inicia criar_arquivo_ofertas_vs_depara_ticket.')
+    logging.info('Inicia criar_arquivo_ofertas_vs_depara_ticket.')
 
-	comando_sql = f'exec dbo.SP_PC_LISTAR_OFERTAS_VS_DEPARA_RC_VAR {AAAAMM}'
-	conexao = criar_conexao()
-	cursor = conexao.cursor()
-	df=pd.read_sql(comando_sql, conexao)
-	os.makedirs('temp', exist_ok=True)
-	df.to_csv(f'temp/oferta_vs_depara_ticket_var_{AAAAMM}.csv', sep=';', decimal=',') 
+    comando_sql = f'exec dbo.SP_PC_LISTAR_OFERTAS_VS_DEPARA_RC_VAR {AAAAMM}'
+    conexao = criar_conexao()
+    cursor = conexao.cursor()
+    df = pd.read_sql(comando_sql, conexao)
+    os.makedirs('temp', exist_ok=True)
+    df.to_csv(
+        f'temp/oferta_vs_depara_ticket_var_{AAAAMM}.csv', sep=';', decimal=',')
 
-	comando_sql = f'exec dbo.SP_PC_LISTAR_OFERTAS_VS_DEPARA_RC_EMP {AAAAMM}'
-	conexao = criar_conexao()
-	cursor = conexao.cursor()
-	df=pd.read_sql(comando_sql, conexao)
-	os.makedirs('temp', exist_ok=True)
-	df.to_csv(f'temp/oferta_vs_depara_ticket_emp_{AAAAMM}.csv', sep=';', decimal=',')
+    comando_sql = f'exec dbo.SP_PC_LISTAR_OFERTAS_VS_DEPARA_RC_EMP {AAAAMM}'
+    conexao = criar_conexao()
+    cursor = conexao.cursor()
+    df = pd.read_sql(comando_sql, conexao)
+    os.makedirs('temp', exist_ok=True)
+    df.to_csv(
+        f'temp/oferta_vs_depara_ticket_emp_{AAAAMM}.csv', sep=';', decimal=',')
 
-	logging.info('FIM criar_arquivo_ofertas_vs_depara_ticket.')
+    logging.info('FIM criar_arquivo_ofertas_vs_depara_ticket.')
 
-	
+
 def main():
-	logging.info('Inicio da Execucao')
-	# Seu código principal começa aqui
+    logging.info('Inicio da Execucao')
+    # Seu código principal começa aqui
 
-	opcaoSelecionada = 0
-	while opcaoSelecionada != 13:
-		opcaoSelecionada = menu()
-		if opcaoSelecionada == '1':
-			print('opção 1 selecionada...(Verificando duplicidades na BOV - consultar log)')
-			roda_verifica_duplicidade_bov()
-			a = input('Tecle qualquer tecla para continuar...')
-	
-		elif opcaoSelecionada == '2':
-			print('opção 2 selecionada...(Calcular tendência HoltWinters)')
-			roda_modelo_tendencia()
-			a = input('Tecle qualquer tecla para continuar...')
-	
-		elif opcaoSelecionada == '3':
-			print('opção 3 selecionada...(Enviar tendência para Operações)')
-			enviar_email_tend_vl_vll_para_operacoes()
-			a = input('Tecle qualquer tecla para continuar...')
-	
-		elif opcaoSelecionada == '4':
-			print('opção 4 selecionada...')
-			a = input('Tecle qualquer tecla para continuar...')
-	
-		elif opcaoSelecionada == '5':
-			print('opção 5 selecionada...(Enviar tendências para tabela de histórico)')
-			tendencias_para_tabela_historico ()
-			a = input('Tecle qualquer tecla para continuar...')
-	
-		elif opcaoSelecionada == '6':
-			print('opção 6 selecionada...(Libera tendências processo legado)')
-			libera_tendencias_processo_legado ()
-			a = input('Tecle qualquer tecla para continuar...')
-	
-		elif opcaoSelecionada == '7':
-			print('opção 7 selecionada...')
-			a = input('Tecle qualquer tecla para continuar...')
-	
-		elif opcaoSelecionada == '8':
-			print('opção 8 selecionada...')
-			a = input('Tecle qualquer tecla para continuar...')
-	
-		elif opcaoSelecionada == '9':
-			print('opção 9 selecionada...(Procedures Receita Contratada)')
-			rotinas_receita_contratada()
-			a = input('Tecle qualquer tecla para continuar...')
-	
-		elif opcaoSelecionada == '10':
-			print('opção 10 selecionada...')
-			criar_arquivo_ofertas_vs_depara_ticket()
-			a = input('Tecle qualquer tecla para continuar...')
-	
-		elif opcaoSelecionada == '11':
-			print('opção 11 selecionada...')
-			a = input('Tecle qualquer tecla para continuar...')
-	
-		elif opcaoSelecionada == '12':
-			print('opção 13 selecionada...(Envia Lista de PDV Outros)')
-			enviar_lista_pdv_outros ()
-			a = input('Tecle qualquer tecla para continuar...')
-	
-		elif opcaoSelecionada == '13':
-			print('Opção 13...SAIR')
-			logging.info('Opcao SAIR selecionada no menu.')
-			break
-		
-		else:
-			print('Opção Inválida')
+    opcaoSelecionada = 0
+    while opcaoSelecionada != 13:
+        opcaoSelecionada = menu()
+        if opcaoSelecionada == '1':
+            print(
+                'opção 1 selecionada...(Verificando duplicidades na BOV - consultar log)')
+            roda_verifica_duplicidade_bov()
+            a = input('Tecle qualquer tecla para continuar...')
+
+        elif opcaoSelecionada == '2':
+            print('opção 2 selecionada...(Calcular tendência HoltWinters)')
+            roda_modelo_tendencia()
+            a = input('Tecle qualquer tecla para continuar...')
+
+        elif opcaoSelecionada == '3':
+            print('opção 3 selecionada...(Enviar tendência para Operações)')
+            enviar_email_tend_vl_vll_para_operacoes()
+            a = input('Tecle qualquer tecla para continuar...')
+
+        elif opcaoSelecionada == '4':
+            print('opção 4 selecionada...')
+            a = input('Tecle qualquer tecla para continuar...')
+
+        elif opcaoSelecionada == '5':
+            print('opção 5 selecionada...(Enviar tendências para tabela de histórico)')
+            tendencias_para_tabela_historico()
+            a = input('Tecle qualquer tecla para continuar...')
+
+        elif opcaoSelecionada == '6':
+            print('opção 6 selecionada...(Libera tendências processo legado)')
+            libera_tendencias_processo_legado()
+            a = input('Tecle qualquer tecla para continuar...')
+
+        elif opcaoSelecionada == '7':
+            print('opção 7 selecionada...')
+            a = input('Tecle qualquer tecla para continuar...')
+
+        elif opcaoSelecionada == '8':
+            print('opção 8 selecionada...')
+            a = input('Tecle qualquer tecla para continuar...')
+
+        elif opcaoSelecionada == '9':
+            print('opção 9 selecionada...(Procedures Receita Contratada)')
+            rotinas_receita_contratada()
+            a = input('Tecle qualquer tecla para continuar...')
+
+        elif opcaoSelecionada == '10':
+            print('opção 10 selecionada...')
+            criar_arquivo_ofertas_vs_depara_ticket()
+            a = input('Tecle qualquer tecla para continuar...')
+
+        elif opcaoSelecionada == '11':
+            print('opção 11 selecionada...')
+            a = input('Tecle qualquer tecla para continuar...')
+
+        elif opcaoSelecionada == '12':
+            print('opção 13 selecionada...(Envia Lista de PDV Outros)')
+            enviar_lista_pdv_outros()
+            a = input('Tecle qualquer tecla para continuar...')
+
+        elif opcaoSelecionada == '13':
+            print('Opção 13...SAIR')
+            logging.info('Opcao SAIR selecionada no menu.')
+            break
+
+        else:
+            print('Opção Inválida')
 
 
 # Verifique se o script está sendo executado diretamente (não importado)
 if __name__ == "__main__":
-	main()
+    main()
--- auto_lobao/funcoes.py
+++ auto_lobao/funcoes.py
@@ -9,9 +9,10 @@ import win32com.client as win32
 import segredos
 
 user_id = os.getlogin()
-FORMAT = f'%(asctime)s | %(levelname)s | %(filename)s | User: {user_id} | %(message)s'
-logging.basicConfig(level=logging.INFO, filename="logs/auto_lobao.log", format=FORMAT)
-
+FORMAT = f'%(asctime)s | %(levelname)s | %(filename)s | User: {
+    user_id} | %(message)s'
+logging.basicConfig(level=logging.INFO,
+                    filename="logs/auto_lobao.log", format=FORMAT)
 
 
 def criar_conexao():
@@ -25,6 +26,7 @@ def criar_conexao():
     )
     return pyodbc.connect(dados_conexao)
 
+
 def executa_procedure_sql(nome_procedure, param=None):
     conexao = criar_conexao()
 
@@ -37,12 +39,14 @@ def executa_procedure_sql(nome_procedure, param=None):
             cursor.execute(f'SET NOCOUNT ON; EXEC {nome_procedure}')
         conexao.commit()
         logging.info(f'Fim Procedure {nome_procedure}.')
-    
+
     except Exception as e:
-        logging.error(f"Erro ao executar a procedure {nome_procedure}: {e}") #TODO trocar por log
-    
+        logging.error(f"Erro ao executar a procedure {
+                      nome_procedure}: {e}")  # TODO trocar por log
+
     finally:
         conexao.close()
+
 
 def enviaEmailComAnexo(EmailTo, Subject, Body, EmailCC=None, File=None):
     try:
@@ -67,7 +71,9 @@ def enviaEmailComAnexo(EmailTo, Subject, Body, EmailCC=None, File=None):
         email.Send()
         logging.info(f'Email enviado com sucesso! Assunto: {Subject}')
     except Exception as e:
-        logging.error(f"Ocorreu um erro ao enviar o email. Assunto {Subject}: {str(e)}")
+        logging.error(f"Ocorreu um erro ao enviar o email. Assunto {
+                      Subject}: {str(e)}")
+
 
 def executa_arquivo_sql(arquivo_sql):
     conexao = criar_conexao()
@@ -83,9 +89,10 @@ def executa_arquivo_sql(arquivo_sql):
 
     except Exception as e:
         logging.error(f"Erro ao executar o arquivo SQL {arquivo_sql}: {e}")
-    
+
     finally:
         conexao.close()
+
 
 def executar_sql(comando_sql):
     try:
@@ -94,18 +101,21 @@ def executar_sql(comando_sql):
         cursor.execute(comando_sql)
         conexao.commit()
         conexao.close()
-        logging.info(f'Executar SQL executado - codigo executado: {comando_sql}')
+        logging.info(
+            f'Executar SQL executado - codigo executado: {comando_sql}')
     except Exception as e:
         logging.error(f"Ocorreu um erro ao executar o SQL: {str(e)}")
+
 
 def copia_e_renomeia_arquivo(origem, destino):
     try:
         shutil.copy(origem, destino)
-        logging.info(f'Arquivo copiado de {origem} para {destino}') 
+        logging.info(f'Arquivo copiado de {origem} para {destino}')
     except FileNotFoundError:
         logging.error(f'Erro: Arquivo de origem não encontrado em {origem}')
     except Exception as e:
         logging.error(f'Ocorreu um erro ao copiar o arquivo: {str(e)}')
+
 
 def verifica_duplicidade_bov(arquivo_zip, arquivo, campo_check):
     try:
@@ -113,13 +123,17 @@ def verifica_duplicidade_bov(arquivo_zip, arquivo, campo_check):
 
         with zipfile.ZipFile(arquivo_zip, 'r') as zip_file:
             with zip_file.open(arquivo) as txt_file:
-                df = pd.read_csv(txt_file, sep=';', encoding='ANSI', quotechar='"', low_memory=False) 
-        
-        contagem_codigos = df.groupby(campo_check).size().reset_index(name='Contagem')
-        contagem_codigos = contagem_codigos.sort_values(by='Contagem', ascending=False)
+                df = pd.read_csv(txt_file, sep=';', encoding='ANSI',
+                                 quotechar='"', low_memory=False)
 
-        check = contagem_codigos.groupby('Contagem').size().reset_index(name='check')
-        
+        contagem_codigos = df.groupby(
+            campo_check).size().reset_index(name='Contagem')
+        contagem_codigos = contagem_codigos.sort_values(
+            by='Contagem', ascending=False)
+
+        check = contagem_codigos.groupby(
+            'Contagem').size().reset_index(name='check')
+
         filtro_igual_1 = check['Contagem'] == 1
         filtro_maior_1 = check['Contagem'] > 1
 
@@ -128,14 +142,17 @@ def verifica_duplicidade_bov(arquivo_zip, arquivo, campo_check):
         total_registros = soma_igual_1 + soma_maior_1
 
         percentual_duplicados = (soma_maior_1 / total_registros) * 100
-        if percentual_duplicados > 1: #maior que 1pct
-            logging.warning(f'ATENÇAO: Arquivo {arquivo_sem_caminho} com percentual de {percentual_duplicados:.2f}% duplicados')
+        if percentual_duplicados > 1:  # maior que 1pct
+            logging.warning(f'ATENÇAO: Arquivo {arquivo_sem_caminho} com percentual de {
+                            percentual_duplicados:.2f}% duplicados')
 
-        resultado = f'Arquivo: {arquivo_sem_caminho} : temos {soma_maior_1} duplicados e {soma_igual_1} sem duplicadas: {percentual_duplicados:.2f}%'
+        resultado = f'Arquivo: {arquivo_sem_caminho} : temos {soma_maior_1} duplicados e {
+            soma_igual_1} sem duplicadas: {percentual_duplicados:.2f}%'
 
         logging.info(resultado)
 
-        return(percentual_duplicados)
+        return (percentual_duplicados)
 
     except Exception as e:
-        logging.error(f"Ocorreu um erro no arquivo {arquivo_sem_caminho}: {str(e)}")
+        logging.error(f"Ocorreu um erro no arquivo {
+                      arquivo_sem_caminho}: {str(e)}")
