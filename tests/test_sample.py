6f31da64df598908fbc21d35a837b238280ee67b
LuizLobao <luiz@lobao.com>
2023-10-31 11:28:34 -0300

    atualizações

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,18 +1,6 @@
-from auto_lobao import *
 
-
-import pytest
-import pyodbc
 
 def teste_basico_inicial ():
     assert 1==1
-
-@pytest.mark.parametrize('server, db_name, expected_result', [
-    ('SQLPW90DB03\DBINST3, 1443', 'BDintelicanais', True),  # Defina os valores de teste e o resultado esperado
-    ('SQLPW90DB03\DBINST3, 1443', 'BDQualivendas', True),
-    ('invalid_server', 'invalid_db', False),
-])
-def test_criar_conexao(server, db_name, expected_result):
-    assert bool(criar_conexao(server, db_name)) == expected_result
 
 
