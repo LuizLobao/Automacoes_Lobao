d5904defa612c61026370b4cdc3e13125b2ab407
LuizLobao <luiz@lobao.com>
2023-11-10 20:13:45 -0300

    carga de meta TBLRESULTADOS VAR e EMP

--- auto_lobao/metas.py
+++ auto_lobao/metas.py
@@ -157,7 +157,7 @@ def meta_pove_para_csv(config, dataframe):
 def prepara_meta_diaria(config):
     '''
     Abrir o arquivo de metas por dia (CargaMeta_Municipio)
-    Apagar a voluna de TOTAL
+    Apagar a coluna de TOTAL
     Fazer a transposição da tabela, transformando os dias em coluna
     Ajustar o nome de alguns registros nas colunas
     '''
@@ -195,6 +195,9 @@ def prepara_meta_diaria(config):
 
 
 def meta_diaria_para_csv(config, dataframe):
+    '''
+    Receber o dataframe depois de ser preparado e salvar como CSV no diretório de rede.
+    '''
     
     diretorio = config['DEFAULT']['dir_rede_metas']
 
@@ -264,6 +267,11 @@ def carregar_metapove_na_tabela_cdo_metas(config, meta_pove_df):
 
 
 def carregar_metadiaria_para_banco_de_dados(meta_diaria_df):
+    '''
+    Receber o dataframe depois de se tratado.
+    Apagar da tabela de Metas os dados do Ano-Mês do DF antes de inserir os dados novos.
+    Carregar dataframe na tabela de metas.
+    '''
     anomes_df = meta_diaria_df['ANOMES'].iloc[0]
 
     comando_sql = (
@@ -293,3 +301,271 @@ def carregar_metadiaria_para_banco_de_dados(meta_diaria_df):
         schema='dbo',
     )
     logging.info(f'Fim da carga de Meta em : TBL_PC_META_DIARIA_VL_VLL')
+
+
+def prepara_meta_tblResultados(config, arquivo_csv):
+    '''
+    ATENÇÃO: na tblResultados carregamos a VLL como VL
+    '''
+    diretorio = config['DEFAULT']['dir_rede_metas']
+    #arquivo_csv = 'meta_pove_202311.csv'
+    caminho = diretorio + arquivo_csv
+
+    df = pd.read_csv(caminho, sep=';',encoding='utf8')
+    anomes_df = df['DT_ANOMES'].iloc[0]
+
+    df = df.drop(columns=['DS_PRODUTO','DS_UGR','DS_GRUPO_INDICADOR','DS_NUMERO_PEDIDO','DS_NUMERO_PEDIDO_ORIG',
+                          'VL_OFERTA','CD_ACESSO_GPON','CD_CANAL_SAP','CD_SURVEY','CD_CELULA','DS_VELOCIDADE',
+                          'CD_CONTA_FATURA','DS_CAMPANHA','DS_OFERTA','CD_CANAL_SAP_ORI','NO_MUNICIPIO','nu_localidade',
+                          'CD_IDENT_PESSOA','DS_TP_POSSE','DT_REFERENCIA'])
+    
+    df = df.rename(columns={'DS_INDICADOR':'INDBD', 
+                                 'DS_TIPO':'TIPO_INDICADOR',
+                                 'DT_ANOMES':'DATA',
+                                 'Produto':'GRUPO_PLANO',
+                                 'DS_UNIDADE_NEGOCIO':'SEGMENTO',
+                                 'NO_CURTO_TERRITORIO':'FILIAL',
+                                 'DS_CANAL_BOV':'CANAL_FINAL',
+                                 'QTD':'VALOR'
+                                 })
+    #incluir as demais colunas necessárias para subir no banco de dados
+    df['CANAL_BOV'] = df.loc[:,'CANAL_FINAL']
+    df['CANAL_PARA'] = np.nan
+    df['COD_SAP'] = np.nan
+    df['DDD'] = np.nan
+    df['PLANO'] = np.nan
+    df['PACOTE'] = np.nan
+    df['CAMPANHA'] = np.nan
+    df['HL'] = np.nan
+    df['VENDEDOR'] = np.nan
+    df['ARPU'] = np.nan
+    df['PLANO_GERENCIAL'] = np.nan
+    df['ZONA_COMPETICAO'] = np.nan
+    df['PLANO_OFERTA'] = np.nan
+    df['PORTABILIDADE'] = np.nan
+    df['MULTIPRODUTO'] = np.nan
+    df['IND_COMBO'] = np.nan
+    df['PEDIDO_UNICO'] = np.nan
+
+
+     #Filtrar
+    lista = ['NOVOS CLIENTES']
+    df = df[df['DS_DET_INDICADOR'].isin(lista)]
+
+    seg = ['VAREJO']
+    df = df[df['SEGMENTO'].isin(seg)]
+
+    seg = ['VLL','GROSS']
+    df = df[df['INDBD'].isin(seg)]
+
+    df = df[['INDBD','TIPO_INDICADOR','DATA','CANAL_BOV','CANAL_PARA',
+             'COD_SAP','FILIAL','DDD','GRUPO_PLANO','PLANO','SEGMENTO',
+             'PACOTE','VALOR','CANAL_FINAL','CAMPANHA','HL','VENDEDOR',
+             'ARPU','PLANO_GERENCIAL','ZONA_COMPETICAO','PLANO_OFERTA',
+             'PORTABILIDADE','MULTIPRODUTO','IND_COMBO','PEDIDO_UNICO']]
+    
+    df['SEGMENTO'] = df['SEGMENTO'].replace('VAREJO', 'VA')
+    df['INDBD'] = df['INDBD'].replace('VLL', 'VL')
+    
+    df.loc[df['CANAL_FINAL'].str.contains('PAP P', case=False), 'CANAL_FINAL'] = 'PAP Próprio'
+
+    df['CANAL_FINAL'] = df['CANAL_FINAL'].replace('TELEAGENTES TLV NACIONAL', 'TLV PP')
+    df.loc[df['CANAL_BOV'].str.contains('PAP P', case=False), 'CANAL_BOV'] = 'PAP Próprio'
+    df['CANAL_BOV'] = df['CANAL_BOV'].replace('TELEAGENTES TLV NACIONAL', 'TLV PP')
+
+    df.to_csv(diretorio+f'tbl_resultado_{anomes_df}.csv',sep=';',
+        header=True,
+        index=False,
+        decimal=',',
+        mode='w', 
+        encoding='utf8')
+    
+    return(df)
+
+
+def carregar_meta_tblResultados(meta_tblResultados_df):
+    '''
+    Receber o dataframe depois de se tratado.
+    Apagar da tabela de Metas os dados do Ano-Mês do DF antes de inserir os dados novos.
+    Carregar dataframe na tabela de metas.
+    '''
+    
+    print (meta_tblResultados_df.dtypes)
+
+    print('tentando converter')
+    meta_tblResultados_df['VALOR'] = [x.replace(',', '.') for x in meta_tblResultados_df['VALOR']]
+    meta_tblResultados_df['VALOR'] = meta_tblResultados_df['VALOR'].astype(float)
+    
+    print (meta_tblResultados_df.dtypes)
+    
+    anomes_df = meta_tblResultados_df['DATA'].iloc[0]
+
+    comando_sql = (
+        f'delete from dbo.tbl_re_baseresultados where data = {anomes_df}'
+    )
+    executar_sql(comando_sql)
+
+    SERVER_NAME = r'SQLPW90DB03\DBINST3,1443'
+    DATABASE_NAME = 'BDintelicanais'
+    connection_string = f"""
+    Driver={{ODBC Driver 13 for SQl Server}};
+    Server={SERVER_NAME};
+    Database={DATABASE_NAME};
+    Trusted_Connection=yes;
+    """
+    connection_url = URL.create(
+        'mssql+pyodbc', query={'odbc_connect': connection_string}
+    )
+    engine = create_engine(connection_url, module=odbc)
+  
+
+    logging.info(f'Iniciando carga de Meta em : tbl_re_baseresultados')
+    meta_tblResultados_df.to_sql(
+        'tbl_re_baseresultados',
+        engine,
+        if_exists='append',
+        index=False,
+        schema='dbo'
+    )
+    logging.info(f'Fim da carga de Meta em : tbl_re_baseresultados')
+
+
+
+def prepara_meta_tblResultadosEmpresarial(config, arquivo_csv):
+    '''
+    ATENÇÃO: na tblResultadosEmpresarial carregamos a VLL como VL
+    '''
+    diretorio = config['DEFAULT']['dir_rede_metas']
+    #arquivo_csv = 'meta_pove_202311.csv'
+    caminho = diretorio + arquivo_csv
+
+    df = pd.read_csv(caminho, sep=';',encoding='utf8')
+    anomes_df = df['DT_ANOMES'].iloc[0]
+
+    df = df.drop(columns=['DS_PRODUTO','DS_UGR','DS_GRUPO_INDICADOR','DS_NUMERO_PEDIDO','DS_NUMERO_PEDIDO_ORIG',
+                          'VL_OFERTA','CD_ACESSO_GPON','CD_CANAL_SAP','CD_SURVEY','CD_CELULA','DS_VELOCIDADE',
+                          'CD_CONTA_FATURA','DS_CAMPANHA','DS_OFERTA','CD_CANAL_SAP_ORI','NO_MUNICIPIO','nu_localidade',
+                          'CD_IDENT_PESSOA','DS_TP_POSSE','DT_REFERENCIA'])
+    
+    df = df.rename(columns={'DS_INDICADOR':'INDBD', 
+                                 'DS_TIPO':'TIPO_INDICADOR',
+                                 'DT_ANOMES':'DATA',
+                                 'Produto':'GRUPO_PLANO',
+                                 'DS_UNIDADE_NEGOCIO':'SEGMENTO',
+                                 'NO_CURTO_TERRITORIO':'UF_CLIENTE',
+                                 'DS_CANAL_BOV':'CANAL_FINAL',
+                                 'QTD':'VALOR'
+                                 })
+    #incluir as demais colunas necessárias para subir no banco de dados
+    df['CANAL_BOV'] = df.loc[:,'CANAL_FINAL']
+    df['UF_CARTEIRA'] = df.loc[:,'UF_CLIENTE']
+    df['CANAL_PARA'] = np.nan
+    df['COD_SAP'] = np.nan
+    df['DDD'] = np.nan
+    df['PLANO'] = np.nan
+    df['PACOTE'] = np.nan
+    df['CAMPANHA'] = ''
+    df['HL'] = ''
+    df['VENDEDOR'] = np.nan
+    df['ARPU'] = np.nan
+    df['PLANO_GERENCIAL'] = ''
+    df['ZONA_COMPETICAO'] = np.nan
+    df['PLANO_OFERTA'] = ''
+    df['PORTABILIDADE'] = ''
+    df['MULTIPRODUTO'] = np.nan
+    df['IND_COMBO'] = np.nan
+    df['PEDIDO_UNICO'] = np.nan
+    df['GER_VENDAS'] = np.nan
+    df['CANAL_VAREJO'] = np.nan
+
+
+     #Filtrar
+    lista = ['NOVOS CLIENTES']
+    df = df[df['DS_DET_INDICADOR'].isin(lista)]
+
+    seg = ['EMPRESARIAL']
+    df = df[df['SEGMENTO'].isin(seg)]
+
+    seg = ['VLL','GROSS']
+    df = df[df['INDBD'].isin(seg)]
+
+    df = df[['INDBD','TIPO_INDICADOR','DATA','CANAL_BOV','CANAL_PARA',
+             'COD_SAP','UF_CLIENTE','UF_CARTEIRA','DDD','GRUPO_PLANO',
+             'PLANO','SEGMENTO','PACOTE','VALOR','CANAL_FINAL','CAMPANHA',
+             'HL','VENDEDOR','GER_VENDAS','ARPU','PLANO_GERENCIAL',
+             'ZONA_COMPETICAO','PLANO_OFERTA','PORTABILIDADE','IND_COMBO','PEDIDO_UNICO','CANAL_VAREJO']]
+    
+    df['SEGMENTO'] = df['SEGMENTO'].replace('EMPRESARIAL', 'EM')
+    df['INDBD'] = df['INDBD'].replace('VLL', 'VL')
+    
+    df.loc[df['CANAL_FINAL'].str.contains('PAP P', case=False), 'CANAL_FINAL'] = 'PAP Próprio'
+
+    df['CANAL_FINAL'] = df['CANAL_FINAL'].replace('TELEAGENTES TLV NACIONAL', 'TLV PP')
+    df.loc[df['CANAL_BOV'].str.contains('PAP P', case=False), 'CANAL_BOV'] = 'PAP Próprio'
+    df['CANAL_BOV'] = df['CANAL_BOV'].replace('TELEAGENTES TLV NACIONAL', 'TLV PP')
+
+    df.to_csv(diretorio+f'tbl_resultadoemp_{anomes_df}.csv',sep=';',
+        header=True,
+        index=False,
+        decimal=',',
+        mode='w', 
+        encoding='utf8')
+    
+    return(df)
+
+def carregar_meta_tblResultadosEmpresarial(meta_tblResultadosEmp_df):
+    '''
+    Receber o dataframe depois de se tratado.
+    Apagar da tabela de Metas os dados do Ano-Mês do DF antes de inserir os dados novos.
+    Carregar dataframe na tabela de metas.
+    '''
+    
+    print (meta_tblResultadosEmp_df.dtypes)
+
+    print('tentando converter')
+    meta_tblResultadosEmp_df['VALOR'] = [x.replace(',', '.') for x in meta_tblResultadosEmp_df['VALOR']]
+    meta_tblResultadosEmp_df['VALOR'] = meta_tblResultadosEmp_df['VALOR'].astype(float)
+    
+    print (meta_tblResultadosEmp_df.dtypes)
+    
+    anomes_df = meta_tblResultadosEmp_df['DATA'].iloc[0]
+
+    comando_sql = (
+        f'delete from dbo.tbl_re_baseresultados_empresarial where data = {anomes_df}'
+    )
+    executar_sql(comando_sql)
+
+    SERVER_NAME = r'SQLPW90DB03\DBINST3,1443'
+    DATABASE_NAME = 'BDintelicanais'
+    connection_string = f"""
+    Driver={{ODBC Driver 13 for SQl Server}};
+    Server={SERVER_NAME};
+    Database={DATABASE_NAME};
+    Trusted_Connection=yes;
+    """
+    connection_url = URL.create(
+        'mssql+pyodbc', query={'odbc_connect': connection_string}
+    )
+    engine = create_engine(connection_url, module=odbc)
+  
+
+    logging.info(f'Iniciando carga de Meta em : tbl_re_baseresultados_empresarial')
+    meta_tblResultadosEmp_df.to_sql(
+        'tbl_re_baseresultados_empresarial',
+        engine,
+        if_exists='append',
+        index=False,
+        schema='dbo'
+    )
+    logging.info(f'Fim da carga de Meta em : tbl_re_baseresultados_empresarial')
+
+
+config = configparser.ConfigParser()
+config.read('auto_lobao/config.ini', encoding='utf-8')
+arquivo_csv = 'meta_pove_202311.csv'
+
+#meta_tblResultados_df = prepara_meta_tblResultados(config, arquivo_csv)
+#carregar_meta_tblResultados(meta_tblResultados_df)
+
+#meta_tblResultadosEmp_df = prepara_meta_tblResultadosEmpresarial(config, arquivo_csv)
+#carregar_meta_tblResultadosEmpresarial(meta_tblResultadosEmp_df)
